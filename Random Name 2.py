724fc977af23439f492475f7ad9df45657a8c4ac
Olawale Iyunade (INSS505-191) <iyunadeo0715@students.bowiestate.edu>
2023-10-02 17:50:05 -0400

    Adding changes to file

---
+++ Random Name 2.py
@@ -0,0 +1,5 @@
+input1 = 5
+input2 = 6
+input3 = 7
+output = input1*input2*input3
+print(output)
---
+++ Random Name 3.py
@@ -0,0 +1,7 @@
+input1 = 5
+input2 = 6
+input3 = 7
+input4 = 8
+output1 = input1+input2+input3+input4
+output2 = input1*input2*input3*input4
+print(output1,output2)
