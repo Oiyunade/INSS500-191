1d83f64546915042915f17ce6aa1194fe467a02c
Olawale Iyunade (INSS505-191) <iyunadeo0715@students.bowiestate.edu>
2023-10-23 19:00:03 -0400

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	Lab 2 part B.py

--- Lab 3.py
+++ Lab 3.py
@@ -1,6 +1,6 @@
 number1= input('Enter number 1')
 number2= input('Enter number 2')
-number3= input('Enter number3')
+number3= input('Enter number 3')
 print(number1,number2)
 sum=number1+number2
 sum=int(number1)+int(number2)
@@ -28,5 +28,18 @@ print(multiply)
 #division=(number1)/(number2)/(number3)
 division=int(number1)/int(number2)/int(number3)
 print(division)
+#import math library
+import math
+print(math.pow(9,3))
+print (math.cos(50))
+print(math.log(2))
+print(math.log1p(2.7183))
+print (math.cos(50))
+print (math.atan(0.39))
+print (math.cos(50))
+print (math.sin(50))
+print(math.expm1(50))
+print(math.factorial(50))
+
 
 
